44b1e9fbbe84c0a21a400412ac5b43b140769017
mattholamieux <matthew.nelson@thelastmile.org>
2020-09-14 14:29:08 -0700

    updated with web-server folder

--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -4,9 +4,9 @@ const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibWF0dGhvbGFtaWV1eCIsImEiOiJja2UwbzY1Y3AyZWx4MnhtY2N6OTVxanMzIn0.hAGScSSOzQ68RRBoeGRXww&limit=1';
     request({url, json: true}, (error, {body}) => {
         if (error) {
-            callback('unable to connect to location services');
+            callback('unable to connect to location services', undefined);
         } else if (body.features.length === 0){
-            callback('unable to find location');
+            callback('unable to find location', undefined);
         } else {
             callback(undefined, {
                 latitude: body.features[0].center[1],
